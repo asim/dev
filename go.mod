6d0109e322980cc702e1d6763dd70978f6c17022
Janos Dobronszki <dobronszki@gmail.com>
2020-10-20 10:34:10 +0200

    Bunch o improvements

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/micro/dev
 
 go 1.14
 
-require github.com/micro/micro/v3 v3.0.0-beta.6.0.20201016094841-ca8ffd563b2b // indirect
+require github.com/micro/micro/v3 v3.0.0-beta.6.0.20201016094841-ca8ffd563b2b
--- model/model.go
+++ model/model.go
@@ -6,11 +6,15 @@
 package model
 
 import (
+	"encoding/json"
+	"fmt"
+
 	"github.com/micro/micro/v3/service/store"
 )
 
 const (
 	queryTypeEq = "eq"
+	indexTypeEq = "eq"
 )
 
 type db struct {
@@ -21,22 +25,85 @@ type db struct {
 }
 
 func (d *db) Save(instance interface{}) error {
+	// @todo replace this hack with reflection
+	js, err := json.Marshal(instance)
+	if err != nil {
+		return err
+	}
+	m := map[string]interface{}{}
+	err = json.Unmarshal(js, &m)
+	if err != nil {
+		return err
+	}
+	id, ok := m["ID"].(string)
+	if !ok || len(id) == 0 {
+		id, ok = m["id"].(string)
+		if !ok || len(id) == 0 {
+			return fmt.Errorf("ID of objects must marshal to JSON key 'ID' or 'id'")
+		}
+	}
+	err = d.store.Write(&store.Record{
+		Key:   id,
+		Value: js,
+	})
+	if err != nil {
+		return err
+	}
+	for _, index := range d.indexes {
+		err = d.store.Write(&store.Record{
+			Key:   indexToSaveKey(index, id, m),
+			Value: js,
+		})
+		if err != nil {
+			return err
+		}
+	}
 	return nil
 }
 
-func (d *db) List(resultPointer interface{}) error {
+func (d *db) List(query Query, resultSlicePointer interface{}) error {
+	for _, index := range d.indexes {
+		if indexMatchesQuery(index, query) {
+			recs, err := d.store.Read(queryToListKey(query), store.ReadPrefix())
+			if err != nil {
+				return err
+			}
+			// @todo speed this up with an actual buffer
+			jsBuffer := []byte("[")
+			for _, rec := range recs {
+				jsBuffer = append(jsBuffer, rec.Value...)
+			}
+			jsBuffer = append(jsBuffer, []byte("]")...)
+			return json.Unmarshal(jsBuffer, resultSlicePointer)
+		}
+	}
+	return fmt.Errorf("Query type %v, field %v does not match any indexes", query.Type, query.FieldName)
+}
 
+func indexMatchesQuery(i Index, q Query) bool {
+	if i.Type == q.Type && i.Ordering == q.Ordering {
+		return true
+	}
+	return false
+}
+
+func queryToListKey(q Query) string {
+	return fmt.Sprintf("by%v:%v", q.FieldName, q.Value)
+}
+
+func indexToSaveKey(i Index, id string, m map[string]interface{}) string {
+	return fmt.Sprintf("by%v:%v", i.FieldName, id)
 }
 
 // DB represents a place where data can be saved to and
 // queried from.
 type DB interface {
 	Save(interface{}) error
-	List(Query) ([]interface{}, error)
+	List(query Query, resultPointer interface{}) error
 }
 
 func NewDB(store store.Store, entity interface{}, indexes []Index) DB {
-	return db{
+	return &db{
 		store, indexes, entity, nil,
 	}
 }
@@ -53,13 +120,27 @@ func Indexes(indexes ...Index) []Index {
 
 // ByEq constructs an equiality index on `fieldName`
 func ByEq(fieldName string) Index {
-
+	return Index{
+		FieldName: fieldName,
+		Type:      indexTypeEq,
+	}
 }
 
 type Query struct {
 	Type      string
 	FieldName string
 	Value     interface{}
+	// false = ascending order, true = descending order
+	Ordering bool
+}
+
+func (q Query) Ord(desc bool) Query {
+	return Query{
+		Type:      q.Type,
+		FieldName: q.FieldName,
+		Value:     q.Value,
+		Ordering:  desc,
+	}
 }
 
 // Eq is an equality query by `fieldName`
