390416545a3f6502cb4a1fcbeda03916ec70d880
Asim Aslam <asim@aslam.me>
2023-07-10 20:25:28 +0100

    Update environment.prod.ts

--- blog/frontend/src/environments/environment.prod.ts
+++ blog/frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  m3oAddress: "https://api.m3o.dev",
+  m3oAddress: "https://api.m3o.com",
   // Replace this before deploying with an actual namespace
   m3oNamespace: "micro"
 };
