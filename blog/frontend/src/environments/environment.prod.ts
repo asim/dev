3f1912d071b67cc7b47937345ffb9b380026e3f2
asim <asim@aslam.me>
2023-07-10 21:58:19 +0100

    reset

--- blog/frontend/src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  microAddress: "https://api.m3o.com",
-  // Replace this before deploying with an actual namespace
-  microNamespace: "micro"
-};
--- go.mod
+++
@@ -1,8 +0,0 @@
-module github.com/micro/dev
-
-go 1.14
-
-require (
-	github.com/gofrs/uuid v4.0.0+incompatible
-	micro.dev/v4 v4.0.0-20230710120043-bdb1ee096b24
-)
